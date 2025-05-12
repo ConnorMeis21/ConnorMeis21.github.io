c845eebb9d1d1c3948b16fe3dbe0959e5a97201e
ConnorMeis21 <cmeisner18@comcast.net>
2025-05-12 19:44:41 -0400

    Update scriptt.js

--- js/scriptt.js
+++ js/scriptt.js
@@ -3,3 +3,11 @@ const favteam= document.querySelector("#favteam");
 favteam.onclick = function() {
     alert("You say your favorite team!");
 };
+function greetUser() {
+    let name = document.getElementById("nameInput").value;
+    if (name.trim() === "") {
+      document.getElementById("message").innerHTML = "Please enter your name.";
+    } else {
+      document.getElementById("message").innerHTML = "Hello, " + name + "!";
+    }
+  }
